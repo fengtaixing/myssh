c2fb9116e815761fc169ee7dc7c700e571f73929
冯太星 <847317487@qq.com>
2020-05-16 21:54:40 +0800

    1

--- demo_git/src/main/java/com/itheima/demo/User.java
+++ demo_git/src/main/java/com/itheima/demo/User.java
@@ -1,4 +1,13 @@
 package com.itheima.demo;
 
 public class User {
+    private int id ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
